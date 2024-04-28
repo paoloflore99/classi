db9d7a75ec6ec376cb2d4fdc0151226ee451da89
paolo flore <paoloflore937@gmail.com>
2024-04-28 21:51:30 +0200

    Aggiungere i file di progetto.

---
+++ ConsoleApp7/Utenti.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp7
+{
+    internal class Utenti
+    {
+        public string Nome {  get; set; }
+        public string Cognome { get; set; }
+        public string Email { get; set; }
+        public string Password { get; set; }
+        public int Numero { get; set; }
+    }
+}
